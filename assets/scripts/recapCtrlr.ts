e1b733385cf6d7ca28549565158e6f97491fb145
habibeshadow <habibe@kayfo.games>
2022-08-04 00:52:56 +0000

    - Designing recap Screen

--- assets/scripts/recapCtrlr.ts
+++ assets/scripts/recapCtrlr.ts
@@ -9,28 +9,9 @@ export class recapCtrlr extends Component {
 
     }
 
-    play()
+    continue()
     {
-        //New only if not current new one
-        if(find('stateManager').getComponent(stateManager).m_playerData.get() == '')
-        {
-            let p : PlayerData = {
-                name: 'Lamine',
-                avatar: '',
-                score: 0,
-                globalScore: 0,
-                coins: 0,
-                pq: 0,
-                progression: {
-                    levelIndex: 0,
-                    roundIndex: 0,
-                    gameIndex: 0,
-                    bestScore: 0
-                }
-            };
-            find('stateManager').getComponent(stateManager).setPlayerData(p);
-        }
-
+       
         let playerData : PlayerData = JSON.parse(find('stateManager').getComponent(stateManager).m_playerData.get())
         let gameStruct : GameStruct = JSON.parse(find('stateManager').getComponent(stateManager).m_gameStruct.get())
 
@@ -42,7 +23,8 @@ export class recapCtrlr extends Component {
         console.log(nextGame);
 
         setTimeout(() => {
-            director.loadScene("quizScene");
+            let gameScene = nextGame + "Scene";
+            director.loadScene(gameScene);
         }, 200);
 
     }

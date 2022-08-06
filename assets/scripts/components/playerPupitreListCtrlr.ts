822d5dd48cbf619830a16b7a6d9a970522017c14
habibeshadow <habibe@kayfo.games>
2022-08-06 14:49:33 +0000

    - Recap done (Players Avatar, Score, elimination 'Trophy wip')

--- assets/scripts/components/playerPupitreListCtrlr.ts
+++ assets/scripts/components/playerPupitreListCtrlr.ts
@@ -1,5 +1,8 @@
-import { _decorator, Component, Node, Prefab } from 'cc';
+import { _decorator, Component, Node, Prefab, instantiate, Sprite, Label, find } from 'cc';
+import { stateManager } from '../managers/stateManager';
 import { playerItemSCROB } from '../utils/scrobs';
+import { PlayerData } from '../utils/types';
+import { playerItemsCtrlr } from './playerItemsCtrlr';
 const { ccclass, property } = _decorator;
 
 @ccclass('playerPupitreListCtrlr')
@@ -16,6 +19,28 @@ export class playerPupitreListCtrlr extends Component {
 
     initView()
     {
+        var playerItemsPrefab = instantiate(this.m_playerItemsPrefab);
+        this.m_playerItemSCROBs = playerItemsPrefab.getComponent(playerItemsCtrlr).playerItems;
+
+        // Get PlayersList
+        let _playersListData : [PlayerData] = JSON.parse(find('stateManager').getComponent(stateManager).m_playersListData.get())
+
+
+        for (let i = 0; i < _playersListData.length; i++) {
+
+            let player = _playersListData.find(e=>e.podiumIndex == i);
+
+            const scrob = this.m_playerItemSCROBs[player.index];
+
+            let playerPupitreItem = this.node.children[i];
+
+            // console.log(this.node.children);
+            playerPupitreItem.getChildByPath("playerMask/player").getComponent(Sprite).spriteFrame = scrob.m_avatar;
+            playerPupitreItem.getChildByPath("playerMask/playerName").getComponent(Label).string = scrob.m_name.toString();
+            playerPupitreItem.getChildByPath("playerMask/playerScore").getComponent(Label).string = player.score.toString();
+            
+
+        }
 
     }
 
